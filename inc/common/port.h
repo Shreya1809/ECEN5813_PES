91d90f908e013b32bfb930b949e7f07ffde70130
Miles Frain <miles.frain@colorado.edu>
2018-02-22 00:49:50 -0700

    some comments on port.h

--- inc/common/port.h
+++ inc/common/port.h
@@ -1,6 +1,22 @@
+/**
+ * @file port.h
+ * @brief Library for GPIO and LED management on KL25Z
+ * @author Shreya Chakraborty
+ * @author Miles Frain
+ * @version 1
+ * @date 2018-02-21
+ */
 
-#ifndef PORT_H_
-#define PORT_H_
+/* Todo Questions
+ *
+ * port.h or gpio.h? - gpio.h, led.h
+ * toggle_red_led is redundant compared to macro
+ * macros are likely more efficient than functions
+ * "static" inline not necessary, since we aren't using local static vars in these functions
+ */
+
+#ifndef __PORT_H__
+#define __PORT_H__
 
 #define RGB_RED_PIN (???)
 #define RGB_GREEN_PIN (???)
@@ -18,13 +34,13 @@
 #define RGB_BLUE_OFF() (PORTB_Clear( RGB_BLUE_PIN ))
 #define RGB_BLUE_TOGGLE() (PORTB_Toggle( RGB_BLUE_PIN ))
 
-void GPIO_Configure();
-void Toggle_Red_LED();
-void PORTB_Set(uint8_t bit_num);
-void PORTD_Set(uint8_t bit_num);
-void PORTB_Clear(uint8_t bit_num);
-void PORTD_Clear(uint8_t bit_num);
-void PORTB_Toggle(uint8_t bit_num);
-void PORTD_Toggle(uint8_t bit_num);
+inline void GPIO_Configure();
+inline void Toggle_Red_LED(); // Todo redundant compared to macro?
+inline void PORTB_Set(uint8_t bit_num);
+inline void PORTD_Set(uint8_t bit_num);
+inline void PORTB_Clear(uint8_t bit_num);
+inline void PORTD_Clear(uint8_t bit_num);
+inline void PORTB_Toggle(uint8_t bit_num);
+inline void PORTD_Toggle(uint8_t bit_num);
 
-#endif
+#endif // __PORT_H__
