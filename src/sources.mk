f1193df7667616f175af6a8f65a5badfc71dff9f
Miles Frain <miles.frain@colorado.edu>
2018-02-25 00:14:58 -0700

    Example unit test

--- src/sources.mk
+++ src/sources.mk
@@ -2,12 +2,14 @@ SOURCES_COMMON = debug.c \
 			  data.c\
 			  memory.c \
 			  conversion.c \
-			  circbuf.c\
-			  project1.c \
-			  main.c
+			  circbuf.c \
+			  project1.c
+
+SOURCES_MAIN = main.c
 
 SOURCES_KL25Z_C = system_MKL25Z4.c
 
 SOURCES_KL25Z_S = startup_MKL25Z4.S
 
+SOURCES_TEST = ../test/simple_test.c
 
