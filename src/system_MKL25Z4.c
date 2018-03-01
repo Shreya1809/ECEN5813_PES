3edd4bd039dfb4bbc2a6ea6ed46a0b4b1cc3dff5
Miles Frain <miles.frain@colorado.edu>
2018-03-01 10:26:57 -0700

    updated startup code from b150220 to b150624

--- src/system_MKL25Z4.c
+++ src/system_MKL25Z4.c
@@ -13,7 +13,7 @@
 **
 **     Reference manual:    KL25P80M48SF0RM, Rev.3, Sep 2012
 **     Version:             rev. 2.5, 2015-02-19
-**     Build:               b150220
+**     Build:               b150224
 **
 **     Abstract:
 **         Provides a system configuration function and a global variable that
@@ -97,7 +97,7 @@
  */
 
 #include <stdint.h>
-#include "MKL25Z4.h"
+#include "fsl_device_registers.h"
 
 
 
