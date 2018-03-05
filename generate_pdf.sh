48c1b1e98f518a1ec53495e2dcb1dc66f2062c5f
Miles Frain <miles.frain@colorado.edu>
2018-03-04 23:38:13 -0700

    pdf generation

---
+++ generate_pdf.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# Get file list with
+# tree -if --noreport . > custom_files.txt
+
+# Then manually edit this list
+
+# Then create pdf
+# cat custom_files.txt | xargs cat | text2pdf > prj2.pdf
